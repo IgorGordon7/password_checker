9a096b2ac33e67c809454cab55efe7df9284902e
IgorGordon7 <50045695+IgorGordon7@users.noreply.github.com>
2021-04-29 15:14:59 +0300

    Update checkmypass.py
    add get_passwords_leak_count function to run above all hashes we've received, splited by hash and count and our hash to check

--- checkmypass.py
+++ checkmypass.py
@@ -3,13 +3,22 @@ import hashlib
 
 
 def request_api_data(query_char):
-    url = 'https://api.pwnedpasswords.com/range' + 'password123'
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
     if res.status_code != 200:
         raise  RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
     return res
 
+def get_passwords_leak_count(hashes,hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h,count in hashes:
+        print(h,count)
+
 
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char,tail = sha1password[:5],sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_passwords_leak_count(response,tail)
+
+pwned_api_check('123')

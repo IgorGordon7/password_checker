73e290b46922b666308e2d729658c68aa20a3f16
IgorGordon7 <50045695+IgorGordon7@users.noreply.github.com>
2021-04-29 15:30:36 +0300

    Update checkmypass.py
    Add main function to receive args(passwords) from concole  and finished the job with the comple code

--- checkmypass.py
+++ checkmypass.py
@@ -1,6 +1,6 @@
 import requests
 import hashlib
-
+import sys
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
@@ -12,7 +12,9 @@ def request_api_data(query_char):
 def get_passwords_leak_count(hashes,hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h,count in hashes:
-        print(h,count)
+        if h == hash_to_check:
+            return count
+    return 0
 
 
 def pwned_api_check(password):
@@ -21,4 +23,14 @@ def pwned_api_check(password):
     response = request_api_data(first5_char)
     return get_passwords_leak_count(response,tail)
 
-pwned_api_check('123')
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times hacked....')
+        else:
+            print(f'{password} was not found hacked')
+
+if __name__=='__main__':
+    sys.exit(main(sys.argv[1:]))
